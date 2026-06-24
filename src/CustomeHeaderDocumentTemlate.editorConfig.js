edc1a4b99d8bbb1dbbcb9785755c3790f2adee8f
Rishabh Shandilya <rishabhshandilya@rishilya.com>
2026-06-24 15:00:49 +0530

    widget transfer

---
+++ src/CustomeHeaderDocumentTemlate.editorConfig.js
@@ -0,0 +1,130 @@
+import { hidePropertyIn } from "@mendix/pluggable-widgets-tools";
+
+/**
+ * @typedef Property
+ * @type {object}
+ * @property {string} key
+ * @property {string} caption
+ * @property {string} description
+ * @property {string[]} objectHeaders
+ * @property {ObjectProperties[]} objects
+ * @property {Properties[]} properties
+ */
+
+/**
+ * @typedef ObjectProperties
+ * @type {object}
+ * @property {PropertyGroup[]} properties
+ * @property {string[]} captions
+ */
+
+/**
+ * @typedef PropertyGroup
+ * @type {object}
+ * @property {string} caption
+ * @property {PropertyGroup[]} propertyGroups
+ * @property {Property[]} properties
+ */
+
+/**
+ * @typedef Properties
+ * @type {PropertyGroup}
+ */
+
+/**
+ * @typedef Problem
+ * @type {object}
+ * @property {string} property
+ * @property {("error" | "warning" | "deprecation")} severity
+ * @property {string} message
+ * @property {string} studioMessage
+ * @property {string} url
+ * @property {string} studioUrl
+ */
+
+/**
+ * @param {object} values
+ * @param {Properties} defaultProperties
+ * @param {("web"|"desktop")} target
+ * @returns {Properties}
+ */
+export function getProperties(values, defaultProperties, target) {
+    // Do the values manipulation here to control the visibility of properties in Studio and Studio Pro conditionally.
+    if (values.headerFooter === "header") {
+        hidePropertyIn(defaultProperties, values, "footerContent");
+        hidePropertyIn(defaultProperties, values, "footerHeight");
+    }
+    if (values.headerFooter === "footer") {
+        hidePropertyIn(defaultProperties, values, "headerContent");
+        hidePropertyIn(defaultProperties, values, "headerHeight");
+    }
+    return defaultProperties;
+}
+
+/**
+ * @param {Object} values
+ * @returns {Problem[]} returns a list of problems.
+ */
+export function check(values) {
+    /** @type {Problem[]} */
+    const errors = [];
+
+    const headerHeightValue = values.headerHeight ? Number(values.headerHeight) : 0;
+    if (values.headerFooter === "both" || values.headerFooter === "header") {
+        if (!values.headerHeight || headerHeightValue <= 0) {
+            errors.push({
+                property: "headerHeight",
+                message: "Header height must be set and positive"
+            });
+        }
+
+        if (!values.headerContent || values.headerContent.widgetCount === 0) {
+            errors.push({
+                property: "headerContent",
+                message: "Place content in the header dropzone"
+            });
+        }
+    }
+
+    const footerHeightValue = values.footerHeight ? Number(values.footerHeight) : 0;
+    if (values.headerFooter === "both" || values.headerFooter === "footer") {
+        if (!values.footerHeight || footerHeightValue <= 0) {
+            errors.push({
+                property: "footerHeight",
+                message: "Footer height must be set and positive"
+            });
+        }
+
+        if (!values.footerContent || values.footerContent.widgetCount === 0) {
+            errors.push({
+                property: "footerContent",
+                message: "Place content in the footer dropzone"
+            });
+        }
+    }
+
+    return errors;
+}
+
+// /**
+//  * @param {object} values
+//  * @param {boolean} isDarkMode
+//  * @param {number[]} version
+//  * @returns {object}
+//  */
+// export function getPreview(values, isDarkMode, version) {
+//     // Customize your pluggable widget appearance for Studio Pro.
+//     return {
+//         type: "Container",
+//         children: []
+//     };
+// }
+
+// /**
+//  * @param {Object} values
+//  * @param {("web"|"desktop")} platform
+//  * @returns {string}
+//  */
+// export function getCustomCaption(values, platform) {
+//     return "CustomeHeaderDocumentTemlate";
+// }
---
+++ src/CustomeHeaderDocumentTemlate.jsx
@@ -0,0 +1,71 @@
+import "./ui/CustomeHeaderDocumentTemlate.css";
+
+export function CustomeHeaderDocumentTemlate(props) {
+    const { headerFooter, headerContent, bodyContent, footerContent, headerHeight, footerHeight } = props;
+
+    const className = ["se-document-layout-widget", "document-layout-widget", props.class].filter(Boolean).join(" ");
+
+    // Created based on this blog post:
+    // https://medium.com/@Idan_Co/the-ultimate-print-html-template-with-header-footer-568f415f6d2a
+
+    // General idea is to use a table because the header and footer of a table will be repeated on every page.
+    // However, the table footer on the last page will be shown directly underneath the content.
+    // So a combination is used: A table to reserve space but not render any content.
+    // Render content in the reserved space using position fixed.
+    // Without reserving space using the table, the contents would overflow the header and footer.
+
+    // The header/footer style only contains the height value.
+    const headerStyle = {
+        height: headerHeight + "px"
+    };
+
+    const footerStyle = {
+        height: footerHeight + "px"
+    };
+
+    return (
+        <div className={className}>
+            <table className="layout-table">
+                {(headerFooter === "header" || headerFooter === "both") && (
+                    <thead>
+                        <tr>
+                            <td>
+                                <div className="header-space" style={headerStyle}>
+                                    &nbsp;
+                                </div>
+                            </td>
+                        </tr>
+                    </thead>
+                )}
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className="content">{bodyContent}</div>
+                        </td>
+                    </tr>
+                </tbody>
+                {(headerFooter === "footer" || headerFooter === "both") && (
+                    <tfoot>
+                        <tr>
+                            <td>
+                                <div className="footer-space" style={footerStyle}>
+                                    &nbsp;
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                )}
+            </table>
+            {(headerFooter === "header" || headerFooter === "both") && (
+                <div className="header" style={headerStyle}>
+                    {headerContent}
+                </div>
+            )}
+            {(headerFooter === "footer" || headerFooter === "both") && (
+                <div className="footer" style={footerStyle}>
+                    {footerContent}
+                </div>
+            )}
+        </div>
+    );
+}
